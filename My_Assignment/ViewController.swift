aa5a5c4a4c15e61a172af86107fd5eb0f8069282
oluwakemi <mabkemix84@gmail.com>
2020-10-14 22:55:04 +0100

    Initial Commit

---
+++ My_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My_Assignment
+//
+//  Created by USER on 14/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
